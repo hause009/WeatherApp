a0cd03aa4d8ca7b6a86505b383b34ab25168a02e
Admin <admin@Admins-Mac.local>
2014-06-16 14:56:26 +0400

    Initial Commit

---
+++ WeatherApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WeatherApp
+//
+//  Created by Admin on 16.06.14.
+//  Copyright (c) 2014 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WeatherApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  WeatherApp
+//
+//  Created by Admin on 16.06.14.
+//  Copyright (c) 2014 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
